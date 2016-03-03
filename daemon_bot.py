6e7dc9820c5675ff7824cad37384a2d7310f8919
James Cox <james@actual-experience.com>
2016-03-03 11:37:58 +0000

    daemon logs

--- daemon_bot.py
+++ daemon_bot.py
@@ -11,15 +11,25 @@ redis_connection = redis.from_url(os.environ["REDIS_URL"])
 
 def queue_daemon(queue, rv_ttl=500):
     while 1:
+        print "[daemon]: waiting for instruction..."
         msg = redis_connection.blpop(queue)
-        func, key, args, kwargs = pickle.loads(msg[1])
+        print "[daemon]: received!"
         try:
-            rv = func(*args, **kwargs)
+            func, key, args, kwargs = pickle.loads(msg[1])
         except Exception, e:
-            rv = e
-        if rv is not None:
-            redis_connection.set(key, pickle.dumps(rv))
-            redis_connection.expire(key, rv_ttl)
+            print "[daemon]: failed to unpickle %s" % msg
+        else:
+            try:
+                print "[daemon]: calling %s" % func
+                rv = func(*args, **kwargs)
+                print "[daemon]: complete!"
+            except Exception, e:
+                print "[daemon]: %s" % e
+                rv = e
+            if rv is not None:
+                redis_connection.set(key, pickle.dumps(rv))
+                redis_connection.expire(key, rv_ttl)
+                print "[daemon]: stored return value at %s" % key
 
 
 queue_daemon(QUEUE_NAME)
