49d5322fbf9d147c4dc7c715ee96450309f120a8
Jamie Cox <jamie@potatolondon.com>
2016-10-11 19:42:27 +0100

    random

---
+++ random_album.py
@@ -0,0 +1,24 @@
+from delayed import delayed
+
+
+message = "Today's album of the day from the Doomlist is: {url}"
+error_message = "Odd... Something went wrong."
+url = "https://doomlist.herokuapp.com/slack/random"
+data = {'token': os.environ.get('SLACK_APP_TOKEN')} 
+channel = "C0A8M8B9Q" # announcements
+
+
+@delayed.queue_func
+def produce_album_of_the_day():
+    response = requests.post(url, data=data)
+    if not response.ok or response.content == '':
+        print '[error]: ' + error_message
+    else:
+        response = response.json()
+        response['channel'] = channel
+        response['text'] = message.format(url=response['text'])
+        print response
+
+
+if __name__ == '__main__':
+	produce_album_of_the_day.delay()
