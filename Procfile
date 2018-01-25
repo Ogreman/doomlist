d3779c451988b3acbfec3dd0988df088bba06ad1
ogreman <theogreman@gmail.com>
2018-01-25 08:53:09 +0000

    Clock processes are not suitable for free tier applications

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 web: gunicorn application:application --log-file=-
 worker: python daemon_bot.py
-clock: python clock.py
--- random_album.py
+++ random_album.py
@@ -2,25 +2,22 @@ import os
 
 from albumlist.models import albums as albums_model
 from albumlist.models import DatabaseError
-from apscheduler.schedulers.blocking import BlockingScheduler
 import slacker
 
 
 channel = os.environ.get('AOTD_CHANNEL_ID')
-hour = int(os.environ.get('AOTD_HOUR', 10))
-sched = BlockingScheduler()
 slack_token = os.environ.get('SLACK_OAUTH_TOKEN')
 slack = slacker.Slacker(slack_token)
 
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=hour)
-def scheduled_job():
-    if not enabled or not channel:
+def post_random_album():
+    if not channel or not slack_token:
+        print('[random]: missing environment variables')
         return
     try:
         album = albums_model.get_random_album()
         if album is None:
-            print('[clock]: no random album found')
+            print('[random]: no random album found')
             return
     except DatabaseError as e:
         print('[db]: failed to get random album')
@@ -28,8 +25,9 @@ def scheduled_job():
         return
     else:
         message = f":new_moon_with_face: Today's album of the day is: {album.album_url}"
-        print(f'[clock]: {message}')
+        print(f'[random]: posting random album to {channel}')
         slack.chat.post_message(channel, message, unfurl_links=True)
 
 
-sched.start()
+if __name__ == '__main__':
+    post_random_album()
